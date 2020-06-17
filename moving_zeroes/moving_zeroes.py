e43d9260282e1731479fc5aef6310805ba57ce25
Josue Rodriguez <smexyjosue@gmail.com>
2020-06-17 16:01:24 -0700

    completes logic using naive solution

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,22 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    # if array length is less then 0 return
+    print(arr)
+    if len(arr) < 0:
+        return
+
+    # loop thru the array
+    for idx in range(len(arr)):
+        # hold the index of the values that equal 0
+        if arr[idx] == 0:
+            # record the index values here, if the value == 0
+            arr.append(arr[idx])
+            arr.pop(idx)
+    return arr
 
 
 if __name__ == '__main__':
